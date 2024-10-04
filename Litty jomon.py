c2bdece07a7ee8626807ac478c157b13e89f8e8e
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-10-04 16:06:17 +0530

    Add files via upload

---
+++ Litty jomon.py
@@ -0,0 +1,44 @@
+#print("Hello, \nworld")
+'''
+if 5>2:
+ print("five is greater than two")
+ x=55
+ x=6
+'''
+'''
+x = str(3)
+y = int(3)
+z = float(3)
+print(type(x))
+print(type(y))
+print(type(z))
+'''
+x, y, z = "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "orange", "banana" "cherry"
+x = "python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+'''
+
+
+username = input("Enter username:")
+print("username is: " + username)
+
+
+a = "Hello"
+b = "world"
+c= a + b
+print(c)
+'''
+
+a = "hello world"
+print(a[:5])
+
+
+
+
