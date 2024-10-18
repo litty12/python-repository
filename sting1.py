58ffb0dcef8be5f8a638f6fcc89a6db0bf24b515
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-10-18 15:11:47 +0530

    Update sting1.py

--- sting1.py
+++ sting1.py
@@ -1,3 +1,10 @@
+
+'''
+author:litty
+date:18 10 2024
+description:Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+
+'''
 str_var="hello, world!"
 str_another=str_var[7:12]
 print(str_another)
