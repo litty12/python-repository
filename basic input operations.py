a50ba947a593f2c487b7019b063297552880cc3d
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-10-18 14:48:38 +0530

    Add files via upload

---
+++ basic input operations.py
@@ -0,0 +1,9 @@
+'''
+aUTHOR:lITTY JOMON
+dATE:18 10 2024
+dESCRIPTION:Write a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+'''
+name=input("enter your name:")
+age=input ("enter your age")
+print("Hello,",name+"!You are",age,"years old")
