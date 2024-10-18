57ece99b372f17d1d8cfa380de8e512507654b09
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-10-18 15:58:19 +0530

    Add files via upload

---
+++ pythonaddition.py
@@ -0,0 +1,12 @@
+'''
+author:litty
+date:18 10 2024
+description:Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+'''
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+print("sum=",num1+num2,"Division",num1/num2)
+print("is number1 greater than number 2?:",num1>num2)
+print("are num1 and num2 equal",num1==num2)
+print("Logical AND=:",num1>0 and num2>0)
+print("logical AND=:",num1<0 or num2>0)
