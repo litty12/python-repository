cde95298cc9e08cf2b6b31ce5c544a401f9b97db
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-10-25 14:39:06 +0530

    Add files via upload

---
+++ task 10.py
@@ -0,0 +1,11 @@
+'''
+author Litty jomon
+date 25 10 2024
+'''
+number=int(input("enter a number"))
+if number>0:
+    print(number,"is a positive number")
+elif number<0:
+    print(number,"is a negative number")
+else:
+    print(number,"is a zero")
